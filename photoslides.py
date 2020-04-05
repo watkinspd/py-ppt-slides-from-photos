f1ff249d47dfbafafbbc53bccb9b487616a02fac
Peter Watkins <pdwatkins@me.com>
2020-04-05 11:31:33 -0700

    Better help  info

--- photoslides.py
+++ photoslides.py
@@ -17,16 +17,17 @@ def main(argv):
                                                 "templatefle=",
                                                 "sortbyfilename="])
     except getopt.GetoptError:
-        print ('try photoslides -h ')
+        print ('try python photoslides.py -h ')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('photoslides -i <dirOfImages> -o <output.pptx> -t <template.pptx> -f <sortbyfilename> \n'
-            '-i <dirOfImages> Must be absolute reference to the directory where the images are. Not "\" on end. Default is the current directory. \n'
-            '-o <output.pptx> Name of powerpoint file to generate. Defaults to test.pptx \n'
-            '-t <template.pptx> Source presentation to use. Defaults to ./template.pptx This tool adds the blank slide style which is assumed to \n'
-            ' be 7th in sequence of the master layouts. \n'
-            '-f Sort image files by filename. Default sort is by date modified. \n')
+            print ('photoslides [options]\n'
+            'The following options can be specified:\n'
+            '-i, --imagelocationdirectory=<dirOfImages> Must be absolute reference to the directory where the images are located. Do not unclude "\\" at end. Default is the current directory.\n'
+            '-o, --outputfile=<output.pptx> Name of powerpoint file to generate. Defaults to test.pptx\n'
+            '-t, --templatefile=<template.pptx> Source presentation to use. Defaults to ./template.pptx This tool adds the blank slide style which is assumed to'
+            ' be 7th in sequence of the master layouts.\n'
+            '-f, --sortbyfilename Sort image files by filename. Default sort is by date modified.\n')
             sys.exit()
         elif opt in ("-i", "--imagelocationdirectory"):
             pathtofiles = arg
